0f10ce2c571c53e8d31c7327ef599fb4a19beaf8
TahaSekk <sohailtaha128@gmail.com>
2022-07-22 00:48:11 +0500

    commity

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
-let NameArray=[
-    {
-        Name:'hello',
-        Age:18
+// let NameArray=[
+//     {
+//         Name:'hello',
+//         Age:18
+//     },
+//     {
+//         Name:'ho',
+//         Age:19
+//     },{
+//         Name:'o',
+//         Age:21
+//     },
+//     {
+//         Name:'h',
+//         Age:54
+//     },
+//     {
+//         Name:'f',
+//         Age:25
+//     }
+// ]
+
+
+// let filteronthebassofage=(humanarray)=>{
+// humanarray.forEach((element, index) => {
+//     if (element.Age===25){
+//         NameArray.push(100);
+//     // humanarray.pop();
+//     }
+// });
+// return humanarray;
+// }
+// // console.log(NameArray);
+// let newValue= filteronthebassofage(NameArray);
+// console.log(newValue);
+
+
+
+let ArrayOfObjects=[
+    {Name:'W',
+    Age:10
     },
-    {
-        Name:'ho',
-        Age:19
-    },{
-        Name:'o',
-        Age:21
-    },
-    {
-        Name:'h',
-        Age:54
-    },
-    {
-        Name:'f',
-        Age:25
-    }
+    
+   {Name:'Y',
+        Age:10
+        },
+    {Name:'G',
+    Age:30
+     },
+  {Name:'H',
+     Age:20
+     },
+        
+    
 ]
 
-
-let filteronthebassofage=(humanarray)=>{
-humanarray.forEach((element, index) => {
-    if (element.Age>=2){
-        NameArray.pop();
-    // humanarray.pop();
+let testOFsplice=(Array1)=>{
+Array1.forEach(element => {
+    if(element.Age>=20){
+        Array1.splice(0,1,{Name:'Taha',Age:18})
     }
 });
-return humanarray;
+return Array1;
+
 }
-// console.log(NameArray);
-let newValue= filteronthebassofage(NameArray);
+let newValue=testOFsplice(ArrayOfObjects);
 console.log(newValue);
